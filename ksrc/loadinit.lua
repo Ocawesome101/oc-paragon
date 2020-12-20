198f7a2ffca4c2693a2e77b866114faa5d2dafb7
Ocawesome101 <ocawesome101@gmail.com>
2020-12-20 02:13:02 -0500

    kernel now sets up initial stdio rather than init.  this allows `init=/bin/sh.lua' in the kernel flags to actually, yknow, *work*.

--- ksrc/loadinit.lua
+++ ksrc/loadinit.lua
@@ -2,6 +2,15 @@
 
 kio.dmesg("ksrc/loadinit.lua")
 
+local function pre_run()
+  if k.io.gpu then
+    local vts = k.vt.new(k.io.gpu, k.io.screen)
+    io.input(vts)
+    io.output(vts)
+    k.sched.getinfo():stderr(vts)
+  end
+end
+
 do
   if computer.freeMemory() < 8192 then
     kio.dmesg("NOTE: init may not load; low memory")
@@ -11,7 +20,7 @@ do
   if not ok then
     kio.panic(err)
   end
-  k.sched.spawn(function()ok(k)end, "[init]", 1)
+  k.sched.spawn(function()pre_run()ok(k)end, "[init]", 1)
 end
 
 k.sched.loop()
--- ktmp/kinfo.lua
+++ ktmp/kinfo.lua
@@ -1,6 +1,6 @@
 _G._KINFO = {
   name    = "Paragon",
   version = "0.6.0",
-  built   = "2020/12/18",
+  built   = "2020/12/20",
   builder = "ocawesome101@manjaro-pbp"
 }
