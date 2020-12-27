5cfdeec4d9be87f6cd94b2666008a8a488e6c60d
ocawesome101 <ocawesome101@gmail.com>
2020-12-26 21:19:06 -0500

    did some things with kernel version increases

--- ktmp/kinfo.lua
+++ ktmp/kinfo.lua
@@ -1,6 +1,6 @@
 _G._KINFO = {
   name    = "Paragon",
-  version = "0.7.0-dev",
+  version = "0.8.0-dev",
   built   = "2020/12/26",
-  builder = "ocawesome101@manjaro-pbp"
+  builder = "ocawesome101@"
 }
